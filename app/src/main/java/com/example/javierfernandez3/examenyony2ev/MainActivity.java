088831d3abf4f7220f5907a298ab240e28f0e21c
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-19 10:23:55 +0100

    Introducido: >Implementado FireBaseAdminListener en MainActivityEvents y rellenados los metodos que vamos a usar >Creado un segundo activity de tipo Nav Drawer

--- app/src/main/java/com/example/javierfernandez3/examenyony2ev/MainActivity.java
+++ app/src/main/java/com/example/javierfernandez3/examenyony2ev/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.javierfernandez3.examenyony2ev;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +17,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-class MainActivityEvents{
+class MainActivityEvents implements FireBaseAdminListener{
 
     MainActivity mainActivity;
 
     public MainActivityEvents(MainActivity mainActivity){
         this.mainActivity= mainActivity;
+    }
+
+    @Override
+    public void firebaseAdmin_registerOk(boolean blOk) {
+
+    }
+
+    @Override
+    public void firebaseAdmin_loginOk(boolean blOk) {
+        if(blOk){
+            Log.v("loginOk","TODO CORRECTO"+blOk);
+            //ESTA PARTE DE CODIGO LO QUE HARA ES INICIAR EL SEGUNDO ACTIVITY Y FINALIZAR EL MAIN
+            Intent intent= new Intent(mainActivity,SecondActivity.class);
+            mainActivity.startActivity(intent);
+            mainActivity.finish();
+
+        }else{
+            Log.v("LoginError","TODO MAL"+blOk);
+        }
+    }
+
+    @Override
+    public void firebaseAdmin_ramaDescargada(String rama, DataSnapshot dataSnapshot) {
+
     }
 }
