70d13c484e265770d7ad037a282c2e2ec8601d6b
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-19 10:16:05 +0100

    Introducido: >Referrencia a base de datos y a FireBaseAuth >Creado el listener de FireBaseAdmin

--- app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdmin.java
+++ app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdmin.java
@@ -1,8 +1,30 @@
 package com.example.javierfernandez3.examenyony2ev;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by javier.fernandez3 on 19/02/2018.
  */
 
 public class FireBaseAdmin {
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    //Referencia a la raiz de la base de datos, de ahi partiremos para observar una rama
+    DatabaseReference myRefRaiz;
+
+    public FireBaseAdminListener listener;
+    public FirebaseUser user;
+
+
+    public FireBaseAdmin(){
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRefRaiz=database.getReference();
+    }
+    public void setListener(FireBaseAdminListener listener) {
+        this.listener = listener;
+    }
 }
