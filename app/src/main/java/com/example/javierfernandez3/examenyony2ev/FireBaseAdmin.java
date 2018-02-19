9f3efdff848723b0b87da65249db8b04484c4e12
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-19 10:43:35 +0100

    Introducido: >En FireBaseAdmin hacemos un metodo para iniciar sesion con twitter y dentro hacemos un login de firebase con credenciales los cuales nos los dará twitter

--- app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdmin.java
+++ app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdmin.java
@@ -1,9 +1,19 @@
 package com.example.javierfernandez3.examenyony2ev;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by javier.fernandez3 on 19/02/2018.
@@ -26,5 +36,26 @@ public class FireBaseAdmin {
     }
     public void setListener(FireBaseAdminListener listener) {
         this.listener = listener;
+    }
+
+    public  void Twitter(AuthCredential credential, Activity activity){
+        mAuth.signInWithCredential(credential).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Log.d("firebaselogin", "signInWithEmail:success");
+                    user = FirebaseAuth.getInstance().getCurrentUser();
+                    listener.firebaseAdmin_loginOk(true);
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    listener.firebaseAdmin_loginOk(false);
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+
+
+                }
+            }
+        });
     }
 }
