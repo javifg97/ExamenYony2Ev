2246c245f816b17ea9382a8e6bec1871d5ae677c
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-19 10:17:09 +0100

    Introducido: >Creado mis propios metodos en FireBaseAdminListener

--- app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdminListener.java
+++ app/src/main/java/com/example/javierfernandez3/examenyony2ev/FireBaseAdminListener.java
@@ -1,9 +1,13 @@
 package com.example.javierfernandez3.examenyony2ev;
 
+import com.google.firebase.database.DataSnapshot;
+
 /**
  * Created by javier.fernandez3 on 19/02/2018.
  */
 
 public interface FireBaseAdminListener {
-
+    public void firebaseAdmin_registerOk(boolean blOk);
+    public void firebaseAdmin_loginOk(boolean blOk);
+    public void firebaseAdmin_ramaDescargada(String rama,DataSnapshot dataSnapshot );
 }
