3b5400ca5a138d64cfab1f1a9b71df6bcb7f5098
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-02-19 13:42:55 +0100

    FINAL Introducido: >Opciones con tres iconos

--- app/src/main/java/com/example/javierfernandez3/examenyony2ev/SecondActivity.java
+++ app/src/main/java/com/example/javierfernandez3/examenyony2ev/SecondActivity.java
@@ -137,17 +137,11 @@ public class SecondActivity extends AppCompatActivity implements NavigationView.
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_app) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_google) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_twtter) {
 
         }
 
